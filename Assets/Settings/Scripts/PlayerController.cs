1cdeab4e1852a0fb25f4a6fa24eef8a42217dab2
Johnsonj91 <Jordan33111@gmail.com>
2023-04-10 22:15:38 -0400

    added player movement using new input system

--- Assets/Settings/Scripts/PlayerController.cs
+++ Assets/Settings/Scripts/PlayerController.cs
@@ -5,6 +5,42 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
   [SerializeField] private float moveSpeed = 1f;
+  private PlayerControls playerControls;
+  private Vector2 movement;
+  private Rigidbody2D rb;
+
+  private void Awake() {
+    playerControls = new PlayerControls();
+    rb = GetComponent<Rigidbody2D>();
+  }
+
+  private void OnEnable() {
+    //This is part of the new unity input system
+    playerControls.Enable();
+  }
+
+// update is good for player input
+  private void Update() {
+    PlayerInput();
+
+  }
+
+  private void PlayerInput() {
+    movement = playerControls.Movement.Move.ReadValue<Vector2>();
+
+    
+  }
+
+  // Good for Physics 
+  private void FixedUpdate() {
+    Move();
+  }
+
+  private void Move(){
+    rb.MovePosition(rb.position + movement * (moveSpeed * Time.fixedDeltaTime));
+  }
+
+
 
   
 
